fd26a0d4d78f3a7930783af3b763467dcdac2227
Neya B <neyabalaji@gmail.com>
2025-03-11 19:35:28 +0530

    Set up Django admin panel for Memories, Patients and Caretakers
    Fixed field errors and improved model registration

--- Backend/lumora_api/admin.py
+++ Backend/lumora_api/admin.py
@@ -1,3 +1,32 @@
+"""
 from django.contrib import admin
+from .models import Memory, Patient, Caretaker
 
-# Register your models here.
+admin.site.register(Memory)
+admin.site.register(Patient)
+admin.site.register(Caretaker)
+
+"""
+from django.contrib import admin
+from .models import Memory, Caretaker, Patient
+
+@admin.register(Memory)
+class MemoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_at") 
+    search_fields = ("title", "description")  
+    list_filter = ("created_at",)
+
+@admin.register(Caretaker)
+class CaretakerAdmin(admin.ModelAdmin):
+    list_display = ("name", "get_email")  
+    search_fields = ("name", "email")
+    
+    @admin.display(description="Email")
+    def get_email(self, obj):
+        return obj.user.email
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ("name", "date_of_birth")  
+    search_fields = ("name",)
+    list_filter = ("date_of_birth",)
